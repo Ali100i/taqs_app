78f2df043a962a787c7810a47b8a2bbc6fabc9f4
Ali100i <ali560i@hotmail.com>
2024-05-23 02:49:36 +0300

    Updates

---
+++ precache-manifest.b133f34c83878748dd50596567b96305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d788a103b54266ac9a09f23518c08444",
+    "url": "/taqs_app/index.html"
+  },
+  {
+    "revision": "0b81cf74eb47a320c4c6",
+    "url": "/taqs_app/static/css/main.eac13453.chunk.css"
+  },
+  {
+    "revision": "a7c2dbee8710dbd03bea",
+    "url": "/taqs_app/static/js/2.2b2162b8.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/taqs_app/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "0b81cf74eb47a320c4c6",
+    "url": "/taqs_app/static/js/main.516e0132.chunk.js"
+  },
+  {
+    "revision": "77015e28ecbe41ca7b20",
+    "url": "/taqs_app/static/js/runtime~main.4520855e.js"
+  },
+  {
+    "revision": "245a7cc670900457d2d7109808e50c4e",
+    "url": "/taqs_app/static/media/landscape.245a7cc6.jpg"
+  }
+]);
